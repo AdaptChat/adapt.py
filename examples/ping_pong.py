8b8c3a4b20463a6bb0a15467e10a281a3f47b5d3
jay3332 <40323796+jay3332@users.noreply.github.com>
2023-04-01 11:16:09 -0400

    add examples

---
+++ examples/ping_pong.py
@@ -0,0 +1,24 @@
+"""Example of a simple bot that responds to any message that says "ping" with "pong"."""
+
+import adapt
+
+client = adapt.Client()
+
+
+# Ready event handler. This is called when the client is ready to start receiving events.
+@adapt.once
+@client.event
+async def on_ready(ready):
+    print(f'Ready as {ready.user}')
+
+
+# Message event handler. This is called when a message is received.
+@client.event
+async def on_message(message):
+    # If the message content is "ping", respond with "pong".
+    if message.content == 'ping':
+        await message.channel.send('Pong!')
+
+
+# Run the client with your bot token.
+client.run('token')
